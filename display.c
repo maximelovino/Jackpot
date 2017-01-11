3ade29530527687cc3446ac9ad7132d413a10623
Vince <vincetournier@msn.com>
2017-01-11 16:28:51 +0100

    Started a display and a massive team fight

---
+++ display.c
@@ -0,0 +1,43 @@
+#include "display.h"
+
+void initDisplay(DisplayArgs* args)
+{
+
+}
+
+void displayWheels(DisplayArgs* args)
+{
+	for(int i = 0 ; i < WHEEL_COUNT ; i++)
+	{
+
+	}
+}
+
+void work(DisplayArgs* args)
+{
+	struct timespec start, finish;
+	while(*(args->state) != QUITTING)
+	{
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		printf("\e[2J");
+		if(*(args->state) == WAITING))
+		{
+			printf("Insert a coin to start the game...\n");
+		}
+		else if(*(args->state) == DONE))
+		{
+			printf("Game finished!\n\n");
+			displayWheels(args);
+		}
+		else
+		{
+			printf("Game started!\n\n");
+			displayWheels(args);
+		}
+
+
+		clock_gettime(CLOCK_MONOTONIC, &finish);
+		double seconds_elapsed = finish.tv_sec-start.tv_sec;
+		seconds_elapsed += (finish.tv_nsec-start.tv_nsec)/1000000000.0;
+	}
+}
--- display.h
+++ display.h
@@ -1,6 +1,19 @@
-#ifndef _DISPLAY_H_
-#define _DISPLAY_H_
+#ifndef NUCLEAR_DISPLAY_SHELTER
+#define NUCLEAR_DISPLAY_SHELTER
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "const.h"
 
+typedef struct DispalyArgs_t
+{
+	int** wheelsValue = NULL;
+	State* state = NULL;
+} DisplayArgs;
+
+void initDisplay(DisplayArgs* args);
+void displayWheels(DisplayArgs* args);
+void work(DisplayArgs* args);
 
 #endif /* end of include guard: _DISPLAY_H_ */
